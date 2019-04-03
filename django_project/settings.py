729269a3a7d34d8eb270dad64548fb15ddd54e4b
fabricio.silva1 <fabricio.silva@fernbi.com.br>
2019-04-03 10:38:25 -0300

    adding files

--- django_project/settings.py
+++ django_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_saml2_auth',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -106,6 +107,38 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+SAML2_AUTH = {
+    # Metadata is required, choose either remote url or local file path
+    'METADATA_AUTO_CONF_URL': '[The auto(dynamic) metadata configuration URL of SAML2]',
+    'METADATA_LOCAL_FILE_PATH': '/home/django/django_project/GoogleIDPMetadata-fernbi.com.br.xml',
+
+    # Optional settings below
+    'DEFAULT_NEXT_URL': '/admin',  # Custom target redirect URL after the user get logged in. Default to /admin if not set. This setting will be overwritten if you have parameter ?next= specificed in the login URL.
+    'CREATE_USER': 'TRUE', # Create a new Django user when a new user logs in. Defaults to True.
+    'NEW_USER_PROFILE': {
+        'USER_GROUPS': [],  # The default group name when a new user logs in
+        'ACTIVE_STATUS': True,  # The default active status for new users
+        'STAFF_STATUS': True,  # The staff status for new users
+        'SUPERUSER_STATUS': False,  # The superuser status for new users
+    },
+    'ATTRIBUTES_MAP': {  # Change Email/UserName/FirstName/LastName to corresponding SAML2 userprofile attributes.
+        'email': 'Email',
+        'username': 'UserName',
+        'first_name': 'FirstName',
+        'last_name': 'LastName',
+    },
+    'TRIGGER': {
+        'CREATE_USER': 'path.to.your.new.user.hook.method',
+        'BEFORE_LOGIN': 'path.to.your.login.hook.method',
+    },
+    'ASSERTION_URL': 'https://google.bixtecnologia.com.br', # Custom URL to validate incoming SAML requests against
+    'ENTITY_ID': 'https://google.bixtecnologia.com.br/saml2_auth/acs/', # Populates the Issuer element in authn request
+    'NAME_ID_FORMAT': FormatString, # Sets the Format property of authn NameIDPolicy element
+    'USE_JWT': False, # Set this to True if you are running a Single Page Application (SPA) with Django Rest Framework (DRF), and are using JWT authentication to authorize client users
+    'FRONTEND_URL': 'https://myfrontendclient.com', # Redirect URL for the client if you are using JWT auth with DRF. See explanation below
+}
 
 
 # Static files (CSS, JavaScript, Images)
--- django_project/urls.py
+++ django_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import django_saml2_auth.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^saml2_auth/', include('django_saml2_auth.urls')),
+    url(r'^accounts/login/$', django_saml2_auth.views.signin),
+    url(r'^admin/login/$', django_saml2_auth.views.signin),
 ]
